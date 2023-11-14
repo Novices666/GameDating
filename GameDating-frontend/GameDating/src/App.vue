de4a56068278c16b6c01410ac11d16598e5d6136
novices666 <1601413678@qq.com>
2023-11-14 22:59:33 +0800

    Init project
    add front-end

---
+++ GameDating-frontend/GameDating/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id = 'app'>
+    <BaseLayout />
+  </div>
+</template>
+
+<script setup>
+  import BaseLayout from "./layouts/BaseLayout.vue";
+
+</script>
+<style scoped>
+
+</style>
---
+++ GameDating-frontend/GameDating/src/layouts/BaseLayout.vue
@@ -0,0 +1,43 @@
+<template>
+
+  <van-nav-bar title="标题" left-arrow>
+    <template #right>
+      <van-icon name="search" size="18" />
+    </template>
+  </van-nav-bar>
+  <div>
+
+  </div>
+
+  <van-tabbar @change="onChange">
+    <van-tabbar-item icon="home-o">标签 1</van-tabbar-item>
+    <van-tabbar-item icon="search">标签 2</van-tabbar-item>
+    <van-tabbar-item icon="friends-o">标签 3</van-tabbar-item>
+    <van-tabbar-item icon="setting-o">标签 4</van-tabbar-item>
+  </van-tabbar>
+
+
+
+</template>
+
+<script>
+import {ref} from 'vue';
+import {showToast} from 'vant';
+
+export default {
+  name: "BaseLayout",
+  setup() {
+    const active = ref(0);
+    const onChange = (index) => showToast(`标签 ${index}`);
+    return {
+      onChange,
+    };
+  },
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
